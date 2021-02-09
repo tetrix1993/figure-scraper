fb4736df039a49fb843687645d33af39e4e9fd29
tetrix1993 <tetrix1993@hotmail.com>
2021-02-09 12:09:27 +0800

    Updated Fnex & Union Creative to use multiprocessing

--- figure_scraper/fnex.py
+++ figure_scraper/fnex.py
@@ -1,5 +1,6 @@
 from figure_scraper.website import Website
 import figure_scraper.constants as constants
+from multiprocessing import Pool
 
 
 class Fnex(Website):
@@ -15,18 +16,35 @@ class Fnex(Website):
         cls.init()
         while True:
             print('[INFO] %s Scraper' % cls.title)
-            expr = input('Enter expression (Image IDs): ')
+            expr = input('Enter expression (Product IDs): ')
             if len(expr) == 0:
                 return
-            numbers = cls.get_numbers_from_expression(expr)
-            for number in numbers:
-                id_ = str(number).zfill(3)
-                for i in range(99):
-                    num = str(i + 1).zfill(2)
-                    image_url = cls.image_url_template % (id_, num)
-                    image_name = cls.image_name_template % (id_, num)
-                    result = cls.download_image(image_url, image_name, print_error_message=False)
-                    if result == -1:
-                        if i == 0:
-                            print('[INFO] Product ID %s not found.' % str(number))
-                        break
+            numbers = cls.get_sorted_page_numbers(expr)
+            if len(numbers) == 0:
+                pass
+            elif len(numbers) == 1:
+                cls.process_product_id(numbers[0])
+            else:
+                max_processes = constants.MAX_PROCESSES
+                if max_processes <= 0:
+                    max_processes = 1
+                with Pool(max_processes) as p:
+                    results = []
+                    for number in numbers:
+                        result = p.apply_async(cls.process_product_id, (number,))
+                        results.append(result)
+                    for result in results:
+                        result.wait()
+
+    @classmethod
+    def process_product_id(cls, number):
+        id_ = str(number).zfill(3)
+        for i in range(99):
+            num = str(i + 1).zfill(2)
+            image_url = cls.image_url_template % (id_, num)
+            image_name = cls.image_name_template % (id_, num)
+            result = cls.download_image(image_url, image_name, print_error_message=False)
+            if result == -1:
+                if i == 0:
+                    print('[INFO] Product ID %s not found.' % str(number))
+                break
--- figure_scraper/union_creative.py
+++ figure_scraper/union_creative.py
@@ -1,5 +1,6 @@
 from figure_scraper.website import Website
 import figure_scraper.constants as constants
+from multiprocessing import Pool
 
 
 class UnionCreative(Website):
@@ -18,18 +19,35 @@ class UnionCreative(Website):
             expr = input('Enter expression (Image IDs): ')
             if len(expr) == 0:
                 return
-            numbers = cls.get_numbers_from_expression(expr)
-            for number in numbers:
-                id_ = str(number)
-                for i in range(100):
-                    if i == 0:
-                        num = id_
-                    else:
-                        num = id_ + '_' + str(i)
-                    image_url = cls.image_url_template % (id_, num)
-                    image_name = cls.image_name_template % num
-                    result = cls.download_image(image_url, image_name, print_error_message=False)
-                    if result == -1:
-                        if i == 0:
-                            print('[INFO] Product ID %s not found.' % str(number))
-                        break
+            numbers = cls.get_sorted_page_numbers(expr)
+            if len(numbers) == 0:
+                pass
+            elif len(numbers) == 1:
+                cls.process_image_id(numbers[0])
+            else:
+                max_processes = constants.MAX_PROCESSES
+                if max_processes <= 0:
+                    max_processes = 1
+                with Pool(max_processes) as p:
+                    results = []
+                    for number in numbers:
+                        result = p.apply_async(cls.process_image_id, (number, ))
+                        results.append(result)
+                    for result in results:
+                        result.wait()
+
+    @classmethod
+    def process_image_id(cls, number):
+        id_ = str(number)
+        for i in range(100):
+            if i == 0:
+                num = id_
+            else:
+                num = id_ + '_' + str(i)
+            image_url = cls.image_url_template % (id_, num)
+            image_name = cls.image_name_template % num
+            result = cls.download_image(image_url, image_name, print_error_message=False)
+            if result == -1:
+                if i == 0:
+                    print('[INFO] Product ID %s not found.' % str(number))
+                break
