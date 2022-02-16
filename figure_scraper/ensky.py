adc511d918242eb18f415ed3520bb8a5cd4fb847
tetrix1993 <tetrix1993@hotmail.com>
2022-02-16 14:12:05 +0800

    Updated Ensky

--- figure_scraper/ensky.py
+++ figure_scraper/ensky.py
@@ -9,14 +9,14 @@ class Ensky(Website):
     keywords = ['https://www.enskyshop.com/']
 
     page_prefix = 'https://www.enskyshop.com'
-    product_url_template = page_prefix + '/products/%s'
+    product_url_template = page_prefix + '/products/detail/%s'
 
     @classmethod
     def run(cls):
         cls.init()
         while True:
             print('[INFO] %s Scraper' % cls.title)
-            print('[INFO] Product Page URL is in the format: https://www.enskyshop.com/products/{product_id}')
+            print('[INFO] Product Page URL is in the format: https://www.enskyshop.com/products/detail/{product_id}')
             expr = input('Enter expression (Product IDs): ').strip()
             if len(expr) == 0:
                 return
@@ -43,9 +43,10 @@ class Ensky(Website):
                 except:
                     print('[ERROR] Invalid choice.')
             if evaluate:
+                today = cls.get_today_date()
                 numbers = cls.get_sorted_page_numbers(expr, start_from=1)
                 if len(numbers) == 1:
-                    cls.process_product_page(numbers[0], use_jan)
+                    cls.process_product_page(numbers[0], use_jan, today)
                 elif len(numbers) > 1:
                     max_processes = min(constants.MAX_PROCESSES, len(numbers))
                     if max_processes <= 0:
@@ -53,35 +54,36 @@ class Ensky(Website):
                     with Pool(max_processes) as p:
                         results = []
                         for number in numbers:
-                            result = p.apply_async(cls.process_product_page, (number, use_jan))
+                            result = p.apply_async(cls.process_product_page, (number, use_jan, today))
                             results.append(result)
                         for result in results:
                             result.wait()
 
     @classmethod
-    def process_product_page(cls, product_id, use_jan=False):
+    def process_product_page(cls, product_id, use_jan=False, folder=None):
         id_ = str(product_id)
         product_url = cls.product_url_template % id_
         image_name_prefix = id_
         try:
             soup = cls.get_soup(product_url)
-            divs = soup.find_all('div', class_='subphotoimg')
-            if len(divs) == 0:
+            images = soup.select('.item_visual .slide-item img')
+            if len(images) == 0:
                 print('[ERROR] Product ID %s does not exists.' % id_)
                 return
             if use_jan:
                 jan_code = cls.get_jan_code(soup)
                 if len(jan_code) > 0:
                     image_name_prefix = jan_code
-            num_max_length = len(str(len(divs)))
-            for i in range(len(divs)):
-                image = divs[i].find('img')
-                if image and image.has_attr('src'):
-                    image_url = cls.page_prefix + image['src']
-                    if len(divs) == 1:
+            num_max_length = len(str(len(images)))
+            for i in range(len(images)):
+                if images[i].has_attr('src'):
+                    image_url = cls.page_prefix + images[i]['src']
+                    if len(images) == 1:
                         image_name = image_name_prefix + '.jpg'
                     else:
                         image_name = '%s_%s.jpg' % (image_name_prefix, str(i + 1).zfill(num_max_length))
+                    if folder:
+                        image_name = folder + '/' + image_name
                     cls.download_image(image_url, image_name)
         except Exception as e:
             print('[ERROR] Error in processing %s' % product_url)
@@ -90,9 +92,9 @@ class Ensky(Website):
     @staticmethod
     def get_jan_code(soup):
         result = ''
-        span = soup.find('span', id='product_code_default')
-        if span:
-            text = span.text.strip()
+        divs = soup.select('.ec-productRole__code .product-list-data')
+        if len(divs) > 0:
+            text = divs[0].text.strip()
             if len(text) == 13:
                 result = text
         return result
