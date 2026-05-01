179e278a82010896df235d1a8f440f82940efe71
tetrix1993 <tetrix1993@hotmail.com>
2026-05-02 07:57:44 +0800

    Updated EurekaKuji

--- figure_scraper/eurekakuji.py
+++ figure_scraper/eurekakuji.py
@@ -9,6 +9,7 @@ class EurekaKuji(Website):
 
     product_url_prefix = 'https://eureka-kuji.com/'
     product_url_template = product_url_prefix + 'lp/%s'
+    product_url_template2 = product_url_prefix + 'dlp/%s'
 
     @classmethod
     def run(cls):
@@ -23,18 +24,25 @@ class EurekaKuji(Website):
 
     @classmethod
     def process_product_page(cls, page):
-        page_url = cls.product_url_template % page
-        if not page_url.endswith('/'):
-            page_url += '/'
+        is_valid = False
         try:
-            soup = cls.get_soup(page_url)
-            images = soup.select('.prizeList img[src],.pcOnly img[src],.specialimage img[src]')
-            if len(images) == 0:
-                print(f'[ERROR] {page_url} does not exists.')
-                return
-            for image in images:
-                image_url = page_url + image['src']
-                img_name = image['src'].split('/')[-1]
-                cls.download_image(image_url, page + '/' + img_name)
+            for template in [cls.product_url_template, cls.product_url_template2]:
+                page_url = template % page
+                if not page_url.endswith('/'):
+                    page_url += '/'
+                    soup = cls.get_soup(page_url)
+                    if soup is None:
+                        continue
+                    images = soup.select('.prizeList img[src],.pcOnly img[src],.specialimage img[src]')
+                    if len(images) == 0:
+                        continue
+                    is_valid = True
+                    for image in images:
+                        image_url = page_url + image['src']
+                        img_name = image['src'].split('/')[-1]
+                        cls.download_image(image_url, page + '/' + img_name)
+                    break
+            if not is_valid:
+                print(f'[ERROR] {cls.product_url_template % page} does not exists.')
         except Exception as e:
-            print(f'[ERROR] Error in processing {page_url}: {e}')
+            print(f'[ERROR] Error in processing {page}: {e}')
